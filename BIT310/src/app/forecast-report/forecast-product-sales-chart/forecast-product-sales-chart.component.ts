1b34f5e36d9acf7acfc52bdb48398ae67f89fa50
ChengHannKoi <chengghannkoi@gmail.com>
2020-10-18 17:48:22 +0800

    latest update at forecast

--- BIT310/src/app/forecast-report/forecast-product-sales-chart/forecast-product-sales-chart.component.ts
+++ BIT310/src/app/forecast-report/forecast-product-sales-chart/forecast-product-sales-chart.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForecastProductSalesChartComponent implements OnInit {
 
+  product1 = [
+    {no: "1.", name: "chair 1", sold:"600", revenue:"6000.00MYR"},
+    {no: "2.", name: "Table 1", sold:"150 ↓", revenue:"1500.00MYR ↓"},
+    {no: "3.", name: "Table 2", sold:"150 ↑", revenue:"1500.00MYR ↑"},
+    {no: "4.", name: "Sofa 1", sold:"50 ↑", revenue:"500.00MYR ↑"},
+    {no: "5.", name: "Shelf 1", sold:"50", revenue:"500.00MYR"}
+  ];
+  product2 = [
+    {no: "1.", name: "Desk 1", sold:"15 ↑", revenue:"150.00MYR ↑"},
+    {no: "2.", name: "Shelf 2", sold:"15", revenue:"150.00MYR"},
+    {no: "3.", name: "Shelf 3", sold:"15", revenue:"150.00MYR"},
+    {no: "4.", name: "Carpet 1", sold:"15 ↓", revenue:"1500.00MYR ↓"},
+    {no: "5.", name: "Shelf 4", sold:"15 ↓", revenue:"150.00MYR ↓"}
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
