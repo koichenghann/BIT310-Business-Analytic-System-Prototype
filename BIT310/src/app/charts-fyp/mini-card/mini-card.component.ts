1b76d1327429e82131ba080f19cd23d4eb8b6242
ChengHannKoi <chengghannkoi@gmail.com>
2020-11-15 18:13:14 +0800

    bacsic layout done

--- BIT310/src/app/charts-fyp/mini-card/mini-card.component.ts
+++ BIT310/src/app/charts-fyp/mini-card/mini-card.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
 
 @Component({
   selector: 'app-mini-card',
@@ -6,10 +9,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mini-card.component.scss']
 })
 export class MiniCardComponent implements OnInit {
+  @Input() label: string;
+  @Input() total: string;
+  @Input() percentage: string;
+  @Input() hidechart: boolean;
+
+  chartOptions: {};
+  Highcharts = Highcharts;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.chartOptions = {
+        chart: {
+            type: 'area',
+            backgroundColor: null,
+            borderWidth: 0,
+            margin: [2, 2, 2, 2],
+            height: 50
+        },
+        title: {
+            text: null
+        },
+        subtitle: {
+            text: null
+        },
+        tooltip: {
+            split: true,
+            outside: true
+        },
+        legend: {
+          enabled: false
+        },
+        credits: {
+          enabled: false
+        } ,
+        exporting: {
+          enabled: false
+        },
+        xAxis: {
+          labels: {
+            enabled: false,
+
+          },
+          title: {
+            text: null
+          },
+          startOnTick: false,
+          endOnTick: false,
+          tickOptions: []
+        },
+        yAxis: {
+          labels: {
+            enabled: false,
+
+          },
+          title: {
+            text: null
+          },
+          startOnTick: false,
+          endOnTick: false,
+          tickOptions: []
+        },
+        series: [{
+          data: [71, 78, 39, 66]
+        }]
+    };
+
+    HC_exporting(Highcharts);
+
+    setTimeout(()=>{
+      window.dispatchEvent(
+        new Event('resize')
+      );
+    },300);
+
   }
 
 }
